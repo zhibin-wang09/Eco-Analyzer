6e03d0dafe3a6f11e395e383edef2bc1cf279444
RichardKim503 <Richardkim503@gmail.com>
2024-09-17 00:59:44 -0400

    Added dummy race and age data. Made a set of buttons to allow user to change between AR and NY, along with income, race, and age.

--- client/src/chartData.js
+++ client/src/chartData.js
@@ -145,3 +145,195 @@ export const NY_IncomeVotingData = [
         percentage: 2
     }
 ]
+
+export const AR_RaceVotingData = [
+    {
+        id: 1,
+        race: 'White (D)',
+        percentage: 15
+    },
+    {
+        id: 2,
+        race: 'White (R)',
+        percentage: 55
+    },
+    {
+        id: 3,
+        race: 'Black (D)',
+        percentage: 8
+    },
+    {
+        id: 4,
+        race: 'Black (R)',
+        percentage: 2
+    },
+    {
+        id: 5,
+        race: 'Asian (D)',
+        percentage: 1
+    },
+    {
+        id: 6,
+        race: 'Asian (R)',
+        percentage: 1
+    },
+    {
+        id: 7,
+        race: 'Hispanic (D)',
+        percentage: 4
+    },
+    {
+        id: 8,
+        race: 'Hispanic (R)',
+        percentage: 2
+    },
+    {
+        id: 9,
+        race: 'Other (D)',
+        percentage: 5
+    },
+    {
+        id: 10,
+        race: 'Other (R)',
+        percentage: 7
+    },
+]
+
+export const NY_RaceVotingData = [
+    {
+        id: 1,
+        race: 'White (D)',
+        percentage: 25
+    },
+    {
+        id: 2,
+        race: 'White (R)',
+        percentage: 15
+    },
+    {
+        id: 3,
+        race: 'Black (D)',
+        percentage: 12
+    },
+    {
+        id: 4,
+        race: 'Black (R)',
+        percentage: 3
+    },
+    {
+        id: 5,
+        race: 'Asian (D)',
+        percentage: 10
+    },
+    {
+        id: 6,
+        race: 'Asian (R)',
+        percentage: 8
+    },
+    {
+        id: 7,
+        race: 'Hispanic (D)',
+        percentage: 7
+    },
+    {
+        id: 8,
+        race: 'Hispanic (R)',
+        percentage: 2
+    },
+    {
+        id: 9,
+        race: 'Other (D)',
+        percentage: 10
+    },
+    {
+        id: 10,
+        race: 'Other (R)',
+        percentage: 8
+    },
+]
+
+export const AR_AgeVotingData = [
+    {
+        id: 1,
+        age: '18-25 (D)',
+        percentage: 5
+    },
+    {
+        id: 2,
+        age: '18-25 (R)',
+        percentage: 15
+    },
+    {
+        id: 3,
+        age: '26-45 (D)',
+        percentage: 15
+    },
+    {
+        id: 4,
+        age: '26-45 (R)',
+        percentage: 20
+    },
+    {
+        id: 5,
+        age: '46-65 (D)',
+        percentage: 13
+    },
+    {
+        id: 6,
+        age: '46-65 (R)',
+        percentage: 22
+    },
+    {
+        id: 7,
+        age: '65+ (D)',
+        percentage: 4
+    },
+    {
+        id: 8,
+        age: '65+ (R)',
+        percentage: 6
+    },
+]
+
+export const NY_AgeVotingData = [
+    {
+        id: 1,
+        age: '18-25 (D)',
+        percentage: 20
+    },
+    {
+        id: 2,
+        age: '18-25 (R)',
+        percentage: 5
+    },
+    {
+        id: 3,
+        age: '26-45 (D)',
+        percentage: 25
+    },
+    {
+        id: 4,
+        age: '26-45 (R)',
+        percentage: 15
+    },
+    {
+        id: 5,
+        age: '46-65 (D)',
+        percentage: 12
+    },
+    {
+        id: 6,
+        age: '46-65 (R)',
+        percentage: 8
+    },
+    {
+        id: 7,
+        age: '65+ (D)',
+        percentage: 10
+    },
+    {
+        id: 8,
+        age: '65+ (R)',
+        percentage: 5
+    },
+]
--- client/src/components/BaseChart.tsx
+++ client/src/components/BaseChart.tsx
@@ -1,6 +1,13 @@
 import React, { useState } from 'react';
 import BarChart from './BarChart';
-import { AR_IncomeVotingData, NY_IncomeVotingData } from '../chartData';
+import { 
+    AR_IncomeVotingData,
+    NY_IncomeVotingData,
+    AR_RaceVotingData,
+    NY_RaceVotingData,
+    AR_AgeVotingData,
+    NY_AgeVotingData 
+} from '../chartData';
 
 export default function BaseChart(){
 
@@ -30,9 +37,132 @@ export default function BaseChart(){
         }
     );
 
+    const [AR_RaceData, setAR_RaceData] = useState(
+        {
+          labels: AR_RaceVotingData.map((data) => data.race),
+          datasets: [
+            {
+              label: "Voting Percentage",
+              data: AR_RaceVotingData.map((data) => data.percentage),
+              backgroundColor: ["blue", "red"]
+            }
+          ]
+        }
+    );
+
+    const [NY_RaceData, setNY_RaceData] = useState(
+        {
+          labels: NY_RaceVotingData.map((data) => data.race),
+          datasets: [
+            {
+              label: "Voting Percentage",
+              data: NY_RaceVotingData.map((data) => data.percentage),
+              backgroundColor: ["blue", "red"]
+            }
+          ]
+        }
+    );
+
+    const [AR_AgeData, setAR_AgeData] = useState(
+        {
+          labels: AR_AgeVotingData.map((data) => data.age),
+          datasets: [
+            {
+              label: "Voting Percentage",
+              data: AR_AgeVotingData.map((data) => data.percentage),
+              backgroundColor: ["blue", "red"]
+            }
+          ]
+        }
+    );
+
+    const [NY_AgeData, setNY_AgeData] = useState(
+        {
+          labels: NY_AgeVotingData.map((data) => data.age),
+          datasets: [
+            {
+              label: "Voting Percentage",
+              data: NY_AgeVotingData.map((data) => data.percentage),
+              backgroundColor: ["blue", "red"]
+            }
+          ]
+        }
+    );
+
+    const [AR_or_NY, setAR_or_NY] = useState('AR');
+    const [dataType, setDataType] = useState('income');
+
+    // const handleStateChange = (event) => {
+    //     setWhichState(event.target.value);
+    // }
+
+    const setAR = () => {
+        setAR_or_NY('AR');
+    }
+
+    const setNY = () => {
+        setAR_or_NY('NY');
+    }
+
+    const setToIncome = () => {
+        setDataType('income');
+    }
+
+    const setToRace = () => {
+        setDataType('race');
+    }
+
+    const setToAge = () => {
+        setDataType('age');
+    }
+
+
     return(
-        <div style={{width: 600}}>
-            <BarChart chartData={NY_IncomeData}/>
+        <div>
+            <div>
+                <button onClick={setAR}>
+                    AR
+                </button>
+                <button onClick={setNY}>
+                    NY
+                </button>
+                <button onClick={setToIncome}>
+                    Income
+                </button>
+                <button onClick={setToRace}>
+                    Race
+                </button>
+                <button onClick={setToAge}>
+                    Age
+                </button>
+            </div>
+
+            <div style={{width: 600}}>
+                {(AR_or_NY == 'AR' && dataType == 'income') && (
+                    <BarChart chartData={AR_IncomeData}/>
+                )}
+
+                {(AR_or_NY == 'AR' && dataType == 'race') && (
+                    <BarChart chartData={AR_RaceData}/>
+                )}
+
+                {(AR_or_NY == 'AR' && dataType == 'age') && (
+                    <BarChart chartData={AR_AgeData}/>
+                )}
+
+                {(AR_or_NY == 'NY' && dataType == 'income') && (
+                    <BarChart chartData={NY_IncomeData}/>
+                )}
+
+                {(AR_or_NY == 'NY' && dataType == 'race') && (
+                    <BarChart chartData={NY_RaceData}/>
+                )}
+
+                {(AR_or_NY == 'NY' && dataType == 'age') && (
+                    <BarChart chartData={NY_AgeData}/>
+                )}
+            </div>
         </div>
+        
     )
 }
