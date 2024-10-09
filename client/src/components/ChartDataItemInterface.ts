bc38b462b5e265b87ce8c2722a523e6a98bd184f
RichardKim503 <Richardkim503@gmail.com>
2024-10-09 18:27:47 -0400

    Reverted ChartData interface

--- client/src/components/ChartDataItemInterface.ts
+++ client/src/components/ChartDataItemInterface.ts
@@ -13,9 +13,6 @@ export interface ChartDataItem {
 	income: IncomeGroups[];
 	race: RaceGroups[];
 	age: AgeGroups[];
-	income_label: string;
-	race_label: string;
-	age_label: string;
 }
 
 interface IncomeGroups {
