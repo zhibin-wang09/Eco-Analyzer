cc945adb30bfab563844e15f8126a9cef8fb2131
Zhi Bin <105875767+zhibin-wang09@users.noreply.github.com>
2024-10-10 17:13:21 -0400

    Organized race, age, income data for Arkansas by district

---
+++ pick.js
@@ -0,0 +1,9 @@
+// Script for reading csv files and organizing data into json format
+function readCSV(){
+
+}
+
+// Takes json object and insert it in to the geojson file
+function insertGeoJSON(){
+    
+}
