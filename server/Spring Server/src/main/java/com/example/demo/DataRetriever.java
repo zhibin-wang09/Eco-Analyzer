00bb79f472f3f675dc36f861a4963af67f5ffe7f
RichardKim503 <Richardkim503@gmail.com>
2024-10-14 00:40:37 -0400

    Added new get coordinate function in backend along with 2 different json files for state district

--- server/Spring Server/src/main/java/com/example/demo/DataRetriever.java
+++ server/Spring Server/src/main/java/com/example/demo/DataRetriever.java
@@ -8,6 +8,7 @@ import org.json.JSONObject;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import java.util.Map;
@@ -23,7 +24,7 @@ public class DataRetriever {
 	}
 
 	@CrossOrigin(origins = "http://localhost:3000")
-	@RequestMapping(value = "/getcoordinates", produces = "application/json")
+	@RequestMapping(value = "/oldgetcoordinates", produces = "application/json")
 	public ResponseEntity<Map<String,Object>> getCoordinateData(){
 
 		StringBuilder jsonString = new StringBuilder();
@@ -44,6 +45,33 @@ public class DataRetriever {
 		Map<String, Object> result = obj.toMap();
 		return ResponseEntity.ok(result);
 	}
+
+
+	@CrossOrigin(origins = "http://localhost:3000")
+	@RequestMapping(value = "/getcoordinates/{state}/{boundary}", produces = "application/json")
+	public ResponseEntity<Map<String,Object>> getCoordinates(@PathVariable("state") String state, @PathVariable("boundary") String boundary){
+
+		String fileName = state + "_" + boundary + "_coordinates.json";
+
+		StringBuilder jsonString = new StringBuilder();
+
+		try (InputStream inputStream = getClass().getClassLoader().getResourceAsStream(fileName)){
+			BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
+			String line;
+
+			while ((line = reader.readLine()) != null) {
+				jsonString.append(line);
+			}
+		}
+		catch(IOException e){
+			e.printStackTrace();
+		}
+
+		JSONObject obj = new JSONObject(jsonString.toString());
+		Map<String, Object> result = obj.toMap();
+		return ResponseEntity.ok(result);
+	}
+
 
 	@CrossOrigin(origins = "http://localhost:3000")
 	@RequestMapping(value = "/getchartdata", produces = "application/json")
