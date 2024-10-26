6c74c653d48ddcce2de55a5e566c12296bb1a19c
Zhi Bin <105875767+zhibin-wang09@users.noreply.github.com>
2024-10-26 16:01:00 -0400

    Structure Spring application to more scalable format and split up chart data

---
+++ server/Spring Server/src/main/java/com/example/demo/controller/CoordinateDataController.java
@@ -0,0 +1,40 @@
+package com.example.demo.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import com.example.demo.service.CoordinateDataService;
+
+@RestController
+@CrossOrigin(origins = "http://localhost:3000")
+public class CoordinateDataController {
+
+    private final CoordinateDataService dataService;
+
+    public CoordinateDataController(CoordinateDataService dataService){
+        this.dataService = dataService;
+    }
+
+    @CrossOrigin(origins = "http://localhost:3000")
+	@GetMapping(value = "/oldcoordinates", produces = "application/json")
+	public ResponseEntity<String> getCoordinateData(){
+		return ResponseEntity.ok(dataService.getCoordinateData());
+	}
+
+
+	@CrossOrigin(origins = "http://localhost:3000")
+	@GetMapping(value = "/coordinates/{state}", produces = "application/json")
+	public ResponseEntity<String> getCoordinates(@PathVariable("state") String state){
+		return ResponseEntity.ok(dataService.getCoordinateData(state));
+	}
+
+
+	@CrossOrigin(origins = "http://localhost:3000")
+	@GetMapping(value = "/chartdata/{state}", produces = "application/json")
+	public ResponseEntity<String> getChartData(@PathVariable("state") String state){
+		return ResponseEntity.ok(dataService.getChartData(state));
+	}
+}
---
+++ server/Spring Server/src/main/java/com/example/demo/service/CoordinateDataService.java
@@ -0,0 +1,42 @@
+package com.example.demo.service;
+
+import java.io.InputStream;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class CoordinateDataService {
+
+    private String process(String filename){
+		StringBuilder jsonString = new StringBuilder();
+
+		try (InputStream inputStream = getClass().getClassLoader().getResourceAsStream(filename)){
+			BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
+				String line;
+
+				while ((line = reader.readLine()) != null) {
+					jsonString.append(line);
+				}
+		}
+		catch(IOException e){
+			e.printStackTrace();
+		}
+
+		return jsonString.toString();
+	}
+
+    public String getCoordinateData(){
+        return process("FeatureCollectionCoordinate.json");
+    }
+
+    public String getCoordinateData(String state){
+        return process(state + "_" + "data.json");
+    }
+
+    public String getChartData(String state){
+        return process(state + "_" + "chartdata.json");
+    }
+}
